be43b7660ff2320f1e7bf40d29f6f4bead5e77a0
Dinh Son <buidinhson1209@gmail.com>
2022-04-28 23:11:18 +0700

    Add Folder Picture Cards - 1

---
+++ Picture Cards - 1/assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.card').mousemove(function(e){
+        var x = e.offsetX / 6;
+        var y = e.offsetY / 6;
+        $(this).css('transform','rotateX('+ x +'deg) rotateY('+ y +'deg)');
+        $(this).children('.image').css({'top':y, 'left':x});
+    })
+    $('.card').mouseleave(function(){
+        
+        $(this).css('transform','rotateX('+ 0 +'deg) rotateY('+ 0 +'deg)');
+        $(this).children('.image').css({'top':'0', 'left':'0'});
+    })
+})
